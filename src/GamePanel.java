26409d934d39fcc397e7b37da7d6af4a4b228b1e
League Student <league@iMac-18.attlocal.net>
2019-06-08 10:30:23 -0700

    level 2 game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,86 +9,90 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener, KeyListener{
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer;
-	Rocketship rocket= new Rocketship(250, 700, 50, 50);
-	
+	Rocketship rocket = new Rocketship(250, 700, 50, 50);
+
 	Font titleFont;
-	
+
 	Font emuFont;
-	
+
 	Font regularFont;
-	
+
 	final int MENU_STATE = 0;
 
 	final int GAME_STATE = 1;
 
 	final int END_STATE = 2;
-	
-	int CURRENT_STATE= MENU_STATE;
+
+	int CURRENT_STATE = MENU_STATE;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-	repaint();
-	  if(CURRENT_STATE == MENU_STATE){
+		repaint();
+		if (CURRENT_STATE == MENU_STATE) {
 
-          updateMenuState();
+			updateMenuState();
 
-  }else if(CURRENT_STATE == GAME_STATE){
+		} else if (CURRENT_STATE == GAME_STATE) {
 
-          updateGameState();
+			updateGameState();
 
-  }else if(CURRENT_STATE == END_STATE){
+		} else if (CURRENT_STATE == END_STATE) {
 
-          updateEndState();
+			updateEndState();
 
-  }
-
+		}
 
 	}
+
 	public GamePanel() {
-		timer = new Timer(1000/60, this);
-		titleFont= new Font("Courier", Font.PLAIN, 50);
-	emuFont= new Font("Courier New", Font.ITALIC, 40);
-	regularFont= new Font("Courier New", Font.PLAIN, 30);
+		timer = new Timer(1000 / 60, this);
+		titleFont = new Font("Courier", Font.PLAIN, 50);
+		emuFont = new Font("Courier New", Font.ITALIC, 40);
+		regularFont = new Font("Courier New", Font.PLAIN, 30);
 	}
+
 	public void startGame() {
 		timer.start();
 	}
+
 	@Override
 
-	public void paintComponent(Graphics g){
-	    g.fillRect(10, 10, 100, 100);
-		  if(CURRENT_STATE == MENU_STATE){
+	public void paintComponent(Graphics g) {
+		g.fillRect(10, 10, 100, 100);
+		if (CURRENT_STATE == MENU_STATE) {
 
-	        drawMenuState(g);
+			drawMenuState(g);
 
-	  }else if(CURRENT_STATE == GAME_STATE){
+		} else if (CURRENT_STATE == GAME_STATE) {
 
-	        drawGameState(g);
+			drawGameState(g);
 
-	  }else if(CURRENT_STATE == END_STATE){
+		} else if (CURRENT_STATE == END_STATE) {
 
-	          drawEndState(g);
-	  }
+			drawEndState(g);
+		}
 
-  
+	}
 
-	        }
 	void updateMenuState() {
-		
+
 	}
+
 	void updateGameState() {
-	rocket.update();	
+		rocket.update();
 	}
+
 	void updateEndState() {
-		
+
 	}
+
 	void drawMenuState(Graphics g) {
 		g.setColor(Color.yellow);
 
-		g.fillRect(0, 0, MikeMackEmu.WIDTH, MikeMackEmu.HEIGHT);  
+		g.fillRect(0, 0, MikeMackEmu.WIDTH, MikeMackEmu.HEIGHT);
 		g.setColor(Color.BLACK);
 		g.setFont(titleFont);
 		g.drawString("Emu Roll", 900, 200);
@@ -98,20 +102,22 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		g.drawString("Game created by Jessie Shen", 800, 380);
 		g.drawString("Press ENTER to start the war!!", 750, 700);
 	}
+
 	void drawGameState(Graphics g) {
 		g.setColor(Color.green);
 
-		g.fillRect(0, 0, MikeMackEmu.WIDTH, MikeMackEmu.HEIGHT);  
+		g.fillRect(0, 0, MikeMackEmu.WIDTH, MikeMackEmu.HEIGHT);
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(0, 0, MikeMackEmu.WIDTH, 500);
 		g.setColor(Color.WHITE);
-	g.fillOval(1500, 100, 100, 100);
-	rocket.draw(g);
-}
+		g.fillOval(1500, 100, 100, 100);
+		rocket.draw(g);
+	}
+
 	void drawEndState(Graphics g) {
 		g.setColor(Color.red);
 
-		g.fillRect(0, 0, MikeMackEmu.WIDTH, MikeMackEmu.HEIGHT);    
+		g.fillRect(0, 0, MikeMackEmu.WIDTH, MikeMackEmu.HEIGHT);
 		g.setColor(Color.BLACK);
 		g.setFont(titleFont);
 		g.drawString("༼ つ ಥ_ಥ ༽つ", 900, 200);
@@ -121,44 +127,53 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		g.drawString("mike has won. his head shall haunt you forever", 700, 380);
 		g.drawString("-You earned 0 points-", 750, 700);
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("jump");
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+if(e.getKeyCode()==37) {
+	rocket.left=true;
+}
+if(e.getKeyCode()==39) {
+	rocket.right=true;
+}
+if(e.getKeyCode()==10) {
+	if (CURRENT_STATE == MENU_STATE) {
 
-		if(CURRENT_STATE == MENU_STATE){
-
-            CURRENT_STATE = GAME_STATE;
-
-    }
-
-		else if(CURRENT_STATE == GAME_STATE){
-
-            CURRENT_STATE = END_STATE;
-
-    }
-
-		else if(CURRENT_STATE == END_STATE){
-
-            CURRENT_STATE = MENU_STATE;
-
-    }
-
+		CURRENT_STATE = GAME_STATE;
 
 	}
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+
+	else if (CURRENT_STATE == GAME_STATE) {
+
+		CURRENT_STATE = END_STATE;
+
+	}
+
+	else if (CURRENT_STATE == END_STATE) {
+
+		CURRENT_STATE = MENU_STATE;
+
 	}
 }
 
 
+}
 
-	
-
-
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode()==37) {
+			rocket.left=false;
+		}
+		if(e.getKeyCode()==39) {
+			rocket.right=false;
+		}
+	}
+}
--- src/Rocketship.java
+++ src/Rocketship.java
@@ -2,13 +2,22 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Rocketship extends GameObject{
+	int speed;
+	boolean left;
+	boolean right;
 
 	public Rocketship(int x, int y, int width, int height) {
 		super(x, y, width, height);
+		speed=5;
 		// TODO Auto-generated constructor stub
 	}
 void update() {
-	
+	if(left==true) {
+	x-=speed;	
+	}
+	if( right==true) {
+		x+=speed;
+	}
 }
 void draw(Graphics g) {
 	 g.setColor(Color.BLACK);
